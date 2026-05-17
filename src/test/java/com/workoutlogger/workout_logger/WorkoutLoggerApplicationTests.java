d50abb8f8eb946313481507f93be57a9fb901690
Blake <blakeunde@yahoo.com>
2026-05-16 23:05:21 -0400

    Initial commit with Spring Boot project setup.

---
+++ src/test/java/com/workoutlogger/workout_logger/WorkoutLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workoutlogger.workout_logger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutLoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
