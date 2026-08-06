2ebd365c029fb66281824fce23ed9e6ee84e6ab3
Blake <blakeunde@yahoo.com>
2026-08-06 10:53:43 -0400

    Add name and category search for exercises.

--- src/main/java/com/workoutlogger/workout_logger/repository/ExerciseRepository.java
+++ src/main/java/com/workoutlogger/workout_logger/repository/ExerciseRepository.java
@@ -10,5 +10,7 @@ import java.util.List;
 @Repository
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
 
+    List<Exercise> findByNameContainingIgnoreCase(String name);
 
+    List<Exercise> findByCategoryContainingIgnoreCase(String category);
 }
